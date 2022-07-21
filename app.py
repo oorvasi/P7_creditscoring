a1c57c4db4c0c73758598b544e2533b6e496efcd
oorvasi <60017106+oorvasi@users.noreply.github.com>
2022-07-21 20:55:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-# Library imports
 import pickle
 import pandas as pd
-import json
-from flask import Flask, request
-
+from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+
+@app.route("/")
+def home():
+    return "Prêt à dépenser API"
 
 def load_model():
     # load model
@@ -14,17 +15,12 @@ def load_model():
     clf=pickle.load(pickle_classifier)
     return clf
 
-
-@app.get("/")
-async def root():
-    return {"message": "Prêt à dépenser API"}
-
 @app.get('/{customerID}')
-
 def predict():
     """
     This function is used for making prediction.
     """
+
     # Input data from dashboard request
     request_json = request.get_json()
     print(request_json)
@@ -33,30 +29,33 @@ def predict():
         data.append(request_json[key])
 
     # Loading the model
-    clf = load_model()
+    model = load_model()
 
     # Making prediction
-    y_proba = clf.predict_proba([data])[0][0]
+    y_proba = model.predict_proba([data])[0][0]
 
     # Looking for the customer situation (class 0 or 1)
     # by using the best threshold from precision-recall curve
     y_class = round(y_proba, 2)
-    best_threshold = 0.320
+    best_threshold = 0.36
     customer_class = np.where(y_class > best_threshold, 1, 0)
 
     # Customer score calculation
     score = int(y_class * 100)
 
     # Customer credit application result
-    if customer_class >= threshold_optimized:
-        prediction = 1
+    if customer_class == 1:
+        result = 'à risque'
+        status = 'refusée'
     else:
-        prediction = 0
+        result = 'sans risque'
+        status = 'acceptée'
 
     # API response to the dashboard
     response = json.dumps(
-        {'score': score, 'class': result, 'prediction': prediction})
-    return response
+        {'score': score, 'class': result, 'application': status})
+    return response, 200
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
